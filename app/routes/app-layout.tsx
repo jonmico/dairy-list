d90596fef23f51d41dbad1be68c0195f4b7cf934
Jon Mico <jonmico@gmail.com>
2025-05-10 10:40:36 -0400

    Add some TODOs.

--- app/routes/app-layout.tsx
+++ app/routes/app-layout.tsx
@@ -1,5 +1,8 @@
 import { Outlet } from 'react-router';
 
+// TODO: Find an icon pack.
+// TODO: Think about layout more.
+
 export default function AppLayout() {
   return (
     <div className='h-screen grid grid-rows-[3.5rem_1fr_4rem]'>
