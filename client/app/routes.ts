95b83e679c55dbc8842b2d3827c2a43698e665e4
Jon Mico <jonmico@gmail.com>
2025-07-03 09:14:08 -0400

    - It did not make sense to have add-items in the items folder.

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -13,7 +13,7 @@ export default [
       index('./routes/lists/home.tsx'),
       route('create', './routes/lists/create-list.tsx'),
       route(':id', './routes/lists/list.tsx'),
-      route(':id/add-items', './routes/lists/items/add-items.tsx'),
+      route(':id/add-items', './routes/lists/add-items.tsx'),
       route(':id/expire-items', './routes/lists/items/expire-items.tsx'),
     ]),
   ]),
